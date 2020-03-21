7b48b71007bb26fa79ea0fc3dad021348fcf8d0e
Luca Saba <luca.saba@facile.it>
2020-03-21 20:32:50 +0100

    GitlabTimeTrack-14 Intercept Gitlab Server failure
    Refactor code and correct error on issues update

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -8,11 +8,14 @@ use App\Form\Type\ChooseProjectsType;
 use App\Service\GitlabRequestService;
 use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ObjectRepository;
 use Exception;
+use GuzzleHttp\Exception\ClientException;
 use Knp\Bundle\SnappyBundle\Snappy\Response\PdfResponse;
 use Knp\Snappy\Pdf;
 use Psr\Cache\InvalidArgumentException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -71,7 +74,6 @@ class DefaultController extends AbstractController
      *
      * @param Request $request
      * @return Response
-     * @throws InvalidArgumentException
      */
     public function chooseProjectsAction(Request $request)
     {
@@ -80,68 +82,21 @@ class DefaultController extends AbstractController
         /**
          * We need to fetch all projects from our GitLab server
          */
-        $gitlabProjects = $this->gitlabRequestService->getProjects();
-        $projects = [];
-
-        foreach ($gitlabProjects as $project) {
-            $projects[] = [
-                'gitlabId' => $project->id,
-                'name' => $project->name,
-                'avatarUrl' => $project->avatar_url,
-                'associated' => $repository->findOneBy([
-                    'gitlabId' => $project->id] //We check if the project is already in our database
-                )
-            ];
+        try {
+            $gitlabProjects = $this->gitlabRequestService->getProjects();
+        } catch (InvalidArgumentException | ClientException $e) {
+            $this->addFlash('warning', 'An exception has been thrown: ' . $e->getMessage());
+            return $this->redirectToRoute('home');
         }
 
-        /**
-         * Let's reorder by name the list of projects
-         */
-        usort($projects, function ($val1, $val2) {
-            if($val1['name'] > $val2['name']) {
-                return 1;
-            }
-            return -1;
-        });
-
-        /**
-         * Then we build a form based on the previous data
-         */
+        $projects = $this->orderProjects($gitlabProjects, $repository);
         $form = $this->createForm(ChooseProjectsType::class, null, array(
             'gitlabProjects' => $projects
         ));
 
         $form->handleRequest($request);
         if($form->isSubmitted() && $form->isValid()) {
-            /**
-             * Handling form submission
-             */
-            foreach ($projects as $gitlabProject) {
-                $value = $form->get('project_'.$gitlabProject['gitlabId'])->getData();
-                if($value === false && $gitlabProject['associated'] != null) {
-                    /**
-                     * The value was unchecked but the project is in the DB
-                     * Remove the project
-                     */
-                    $this->entityManager->remove($gitlabProject['associated']);
-                } elseif ($value === true && $gitlabProject['associated'] == null) {
-                    /**
-                     * The value was checked and the project is not in the DB
-                     * Persist the new Project
-                     */
-                    $project = new Project();
-                    $project->setName($gitlabProject['name'])
-                        ->setGitlabId($gitlabProject['gitlabId'])
-                        ->setAvatarUrl($gitlabProject['avatarUrl']);
-                    $this->entityManager->persist($project);
-                }
-                /**
-                 * If $value is unchecked and the project is not associated
-                 * OR $value is checked and project is associated.....
-                 * Nothing to do! Go on.
-                 */
-                $this->entityManager->flush();
-            }
+            $this->handleProjectSelectionForm($projects, $form);
             return $this->redirectToRoute('home');
         }
 
@@ -193,12 +148,11 @@ class DefaultController extends AbstractController
          */
         $gitlabProjectIssues = $this->gitlabRequestService->getProjectsIssues($project);
 
-        $em = $this->getDoctrine()->getManager();
         $updated = 0;
         $inserted = 0;
 
         foreach ($gitlabProjectIssues as $issue) {
-            $newIssue = $em->getRepository(Issue::class)
+            $newIssue = $this->entityManager->getRepository(Issue::class)
                 ->findOneBy(['gitlabId' => $issue->id]);
 
             if ($newIssue == null) {
@@ -213,7 +167,7 @@ class DefaultController extends AbstractController
                     ->setStatus($issue->state)
                     ->setTimeEstimate($issue->time_stats->time_estimate)
                     ->setTotalTimeSpent($issue->time_stats->total_time_spent);
-                $em->persist($newIssue);
+                $this->entityManager->persist($newIssue);
                 $inserted++;
             } else {
                 /**
@@ -228,13 +182,13 @@ class DefaultController extends AbstractController
                         ->setUpdatedAt(new DateTime($issue->updated_at))
                         ->setTimeEstimate($issue->time_stats->time_estimate)
                         ->setTotalTimeSpent($issue->time_stats->total_time_spent);
-                    $em->persist($newIssue);
+                    $this->entityManager->persist($newIssue);
                     $updated++;
                 }
             }
         }
 
-        $em->flush();
+        $this->entityManager->flush();
 
         if($updated > 0) {
             $this->addFlash('success', "$updated issues have been updated.");
@@ -251,8 +205,8 @@ class DefaultController extends AbstractController
      * @Route("/project/{id}/export-to-pdf", name="export_project_to_pdf")
      *
      * @param Project $project
-     * @return \Symfony\Component\HttpFoundation\Response
-     * @throws \Exception
+     * @return Response
+     * @throws Exception
      */
     public function exportProjectToPdfAction(Project $project)
     {
@@ -287,5 +241,68 @@ class DefaultController extends AbstractController
          */
         $date = new DateTime($gitlabTime);
         return new DateTime($date->format('Y-m-d\TH:i:s'));
+    }
+
+    /**
+     * @param array $gitlabProjects
+     * @param ObjectRepository $repository
+     * @return array
+     */
+    private function orderProjects(array $gitlabProjects, ObjectRepository $repository): array
+    {
+        $projects = [];
+        foreach ($gitlabProjects as $project) {
+            $projects[] = [
+                'gitlabId' => $project->id,
+                'name' => $project->name,
+                'avatarUrl' => $project->avatar_url,
+                'associated' => $repository->findOneBy([
+                    'gitlabId' => $project->id] //We check if the project is already in our database
+                )
+            ];
+        }
+
+        /**
+         * Let's reorder by name the list of projects
+         */
+        usort($projects, function ($val1, $val2) {
+            if ($val1['name'] > $val2['name']) {
+                return 1;
+            }
+            return -1;
+        });
+        return $projects;
+    }
+
+    /**
+     * @param array $projects
+     * @param FormInterface $form
+     */
+    private function handleProjectSelectionForm(array $projects, FormInterface $form): void
+    {
+        /**
+         * Handling form submission
+         */
+        foreach ($projects as $gitlabProject) {
+            $value = $form->get('project_' . $gitlabProject['gitlabId'])->getData();
+            if ($value === false && $gitlabProject['associated'] != null) {
+                /**
+                 * The value was unchecked but the project is in the DB
+                 * Remove the project
+                 */
+                $this->entityManager->remove($gitlabProject['associated']);
+            } elseif ($value === true && $gitlabProject['associated'] == null) {
+                /**
+                 * The value was checked and the project is not in the DB
+                 * Persist the new Project
+                 */
+                $project = new Project();
+                $project->setName($gitlabProject['name'])
+                    ->setGitlabId($gitlabProject['gitlabId'])
+                    ->setAvatarUrl($gitlabProject['avatarUrl']);
+                $this->entityManager->persist($project);
+            }
+        }
+        $this->entityManager->flush();
     }
 }
